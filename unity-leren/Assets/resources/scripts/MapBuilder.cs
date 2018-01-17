2303f86bfcda2df667892650ac3d757829880e79
rubendroogh <rubendroogh@hotmail.nl>
2018-01-17 18:02:04 +0100

    hoi

---
+++ unity-leren/Assets/resources/scripts/MapBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBuilder : MonoBehaviour {
+
+    public GameObject[] tiles;
+    public float xPos = 0;
+
+	void Start () {
+        for (int yPos = 0; yPos < 3; yPos++)
+        {
+            Instantiate(tiles[0], new Vector3(xPos, yPos, 0), new Quaternion(0, 0, 0, 0));
+            for (int xPos = 1; xPos < 4; xPos++)
+            {
+                Instantiate(tiles[0], new Vector3(xPos, yPos, 0), new Quaternion(0, 0, 0, 0));
+            }
+        }
+    }
+}

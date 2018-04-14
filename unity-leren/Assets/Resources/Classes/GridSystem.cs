ea829418fb5fe746ce079849a7c72146ec594c32
rubensPC <rubendroogh@hotmail.nl>
2018-04-14 20:44:37 +0200

    Added GridSystem class.

---
+++ unity-leren/Assets/Resources/Classes/GridSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GridSystem {
+
+    public static float multiplier = 0.16f;
+
+    public static Vector2 UnityToGridCoord(Vector2 UnityCoords)
+    {
+        UnityCoords.x = UnityCoords.x * multiplier;
+        UnityCoords.y = UnityCoords.y * multiplier;
+
+        return UnityCoords;
+    }
+
+    public static Vector2 GridToUnityCoord(Vector2 GridCoords)
+    {
+        GridCoords.x = GridCoords.x / multiplier;
+        GridCoords.y = GridCoords.y / multiplier;
+
+        return GridCoords;
+    }
+}

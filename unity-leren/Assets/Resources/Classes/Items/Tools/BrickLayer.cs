cb123a2807c634061a379b917cf258bbf11fe69b
rubensPC <rubendroogh@hotmail.nl>
2018-04-14 20:46:00 +0200

    Added bricklayer item

--- unity-leren/Assets/Resources/Classes/Items/Tools/BrickLayer.cs
+++ unity-leren/Assets/Resources/Classes/Items/Tools/BrickLayer.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Items/Tools/BrickLayer")]
 public class BrickLayer : Item
 {
+    public int maxDistance = 4;
+
     public override void Use()
     {
         base.Use();
         MapBuilder mapBuilder = GameObject.Find("GameManager").GetComponent<MapBuilder>();
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        // convert to coords
         mousePos.x = mousePos.x * (1/0.16f);
         mousePos.y = mousePos.y * (1/0.16f);
 
+        // calc distance between mousepos and player
         float xPosDiff = mousePos.x - holder.xTilePos;
         float yPosDiff = mousePos.y - holder.yTilePos;
 
+        // make positive number
         xPosDiff = (xPosDiff < 0) ? xPosDiff * -1 : xPosDiff;
         yPosDiff = (yPosDiff < 0) ? yPosDiff * -1 : yPosDiff;
 
-        if (xPosDiff <= 4 && yPosDiff <= 4)
+        if (xPosDiff <= maxDistance && yPosDiff <= maxDistance)
         {
             mapBuilder.CreateTile(mapBuilder.tiles[(int)tileType.wall], Mathf.RoundToInt(mousePos.x) , Mathf.RoundToInt(mousePos.y));
-            mapBuilder.CombineTilesInMap();
         }
     }
 }
